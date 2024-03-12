686938dab7ac9db14106627fd06279838941cc4f
Ishan <ishannaik7@gmail.com>
2024-03-12 10:23:20 +0530

    Added App.jsx

---
+++ fireship-react/src/app.jsx
@@ -0,0 +1,9 @@
+function MyComponent(props){
+    return <p> {props.name}</p>
+}
+<MyComponent name="Jeff" />
+
+function MyComponent{{name}}{
+    return <P>{name}</P>
+}
+<MyComponent name ={'JeffD'+ 23} />
