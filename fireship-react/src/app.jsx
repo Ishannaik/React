cf357ee7fad8b171926b15d14e814c3b0e81efc4
Ishan <ishannaik7@gmail.com>
2024-03-12 10:31:39 +0530

    Conditional Rendering

--- fireship-react/src/app.jsx
+++ fireship-react/src/app.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import './style.css';
+
 function MyComponent(props){
     return <p> {props.name}</p>
 }
@@ -7,3 +10,18 @@ function MyComponent{{name}}{
     return <P>{name}</P>
 }
 <MyComponent name ={'JeffD'+ 23} />
+
+// Conditional rendering
+function Conditional ({count}){
+    if(count >5){
+        return <h1>Count is greater than 5</h1>;
+    } else{
+        return <h1>Count is less than 5</h1>;
+    }
+}
+
+//Ternary Operator
+{ count % 2 ===0 ? <h1>Count is even</h1> : <h1>Count is odd</h1>}
+
+//Logical And
+{count && 2 ===0 ? <h1>Count is even</h1>}
