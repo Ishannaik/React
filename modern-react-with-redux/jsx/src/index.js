2daf7c72476f59a8fdac078b770a9a5fd66ed3b0
Ishan <ishannaik7@gmail.com>
2024-03-13 10:11:44 +0530

    Extracting App.js Component

--- modern-react-with-redux/jsx/src/index.js
+++ modern-react-with-redux/jsx/src/index.js
@@ -1,6 +1,7 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 // Get a reference to the div with ID root
 const el = document.getElementById("root");
@@ -9,48 +10,6 @@ const el = document.getElementById("root");
 // Note: This is the updated syntax for React 18
 const root = ReactDOM.createRoot(el);
 
-// Create a component
-function App() {
-  //   let message = "By There!";
-  //   if (Math.random() > 0.5) {
-  //     message = "Hello there!";
-  //  }
-  // const message = {}; //REACT DOESNT SHOW OBJECTS PROPERLY
-  // const message = 123123;
-  // const message = [1, 2, 3];
-  // return <h1>{messages}</h1>;
-  // const date = new Date();
-  // const time = date.toLocaleTimeString();
-  //  return <h1>{time}</h1>;
-  // return <h1>{new Date().toLocaleTimeString()}</h1>;
-  //   const name = "Ishan";
-  //   const age = 20;
-  //   return (
-  //     <h1>
-  //       Hi, my name is {name} and my age is {age}
-  //     </h1>
-  //   );
-  // }
-  // const inputType = "number";
-  // const minValue = -5;
-  // const maxValue = 10;
-  // const listValue = [1, 2, 3];
-  // const styleValue = { backgroundColor: "lightblue" };
-  // const message = "Enter a number";
-  // return (
-  //   <input
-  //     type={inputType}
-  //     min={minValue}
-  //     max={maxValue}
-  //     list={listValue}
-  //     style={styleValue}
-  //     alt={message}
-  //   />
-  // );
-  // return <textarea autoFocus={true} />;
-  // return <input maxLength={5} />;
-  return <input style={{ border: "1px solid red" }} />;
-}
 // Show the component on the screen
 root.render(<App />);
 
@@ -64,3 +23,47 @@ root.render(<App />);
 //   </ul>
 //   </div>
 //    */
+/*
+// Create a component
+// function App() {
+//   let message = "By There!";
+//   if (Math.random() > 0.5) {
+//     message = "Hello there!";
+//  }
+// const message = {}; //REACT DOESNT SHOW OBJECTS PROPERLY
+// const message = 123123;
+// const message = [1, 2, 3];
+// return <h1>{messages}</h1>;
+// const date = new Date();
+// const time = date.toLocaleTimeString();
+//  return <h1>{time}</h1>;
+// return <h1>{new Date().toLocaleTimeString()}</h1>;
+//   const name = "Ishan";
+//   const age = 20;
+//   return (
+//     <h1>
+//       Hi, my name is {name} and my age is {age}
+//     </h1>
+//   );
+// }
+// const inputType = "number";
+// const minValue = -5;
+// const maxValue = 10;
+// const listValue = [1, 2, 3];
+// const styleValue = { backgroundColor: "lightblue" };
+// const message = "Enter a number";
+// return (
+//   <input
+//     type={inputType}
+//     min={minValue}
+//     max={maxValue}
+//     list={listValue}
+//     style={styleValue}
+//     alt={message}
+//   />
+// );
+// return <textarea autoFocus={true} />;
+// return <input maxLength={5} />;
+//   return <input style={{ border: "1px solid red" }} />;
+// }
+*/
