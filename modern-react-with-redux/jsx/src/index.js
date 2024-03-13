e2b3554c28d01103199dd25b90ac79fc2d9a2ab4
Ishan <ishannaik7@gmail.com>
2024-03-13 09:43:09 +0530

    Showing basic JSX Content

--- modern-react-with-redux/jsx/src/index.js
+++ modern-react-with-redux/jsx/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Get a reference to the div with ID root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Tell React to take control of that element
+// Note: This is the updated syntax for React 18
+const root = ReactDOM.createRoot(el);
+
+// Create a component
+function App() {
+  return <h1>Hi There!</h1>;
+}
+
+// Show the component on the screen
+root.render(<App />);
+
+//babeljs.io/repl
+//// /* <div>
+//   <h1>Hi There!</h1>
+//   <ul>
+//     <li>Red</li>
+//     <li>Green</li>
+//     <li>Blue</li>
+//   </ul>
+//   </div>
+//    */
