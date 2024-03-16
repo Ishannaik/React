d51bb3d770f43c5eba26f42ab5c300103ae66d79
Ishan <ishannaik7@gmail.com>
2024-03-16 14:51:50 +0530

    Dont know what happened here

--- modern-react-with-redux/animals/src/App.js
+++ modern-react-with-redux/animals/src/App.js
@@ -1,7 +1,20 @@
 import { useState } from "react";
 
 function App() {
+  // Array Destructuring
+  // function makeArray() {
+  //   return [1, 10, 32, 40];
+  // }
+
+  // // const myArray = makeArray();
+  // // const firstElement = myArray[0];
+  // // const secondElement = myArray[1];
+  // // console.log(firstElement, secondElement);
+  // const [firstElement, secondElement] = makeArray();
+  // console.log(firstElement, secondElement);
+
   const [count, setCount] = useState(0);
+
   const handleClick = () => {
     setCount(count + 1);
   };
