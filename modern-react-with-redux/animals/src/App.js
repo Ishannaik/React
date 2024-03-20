bba86ff6c98ed8bf1a220bb1995b8d205fea3356
Ishan <ishannaik7@gmail.com>
2024-03-20 05:03:02 +0000

    Done for the day

--- modern-react-with-redux/animals/src/App.js
+++ modern-react-with-redux/animals/src/App.js
@@ -1,33 +1,28 @@
+import "./App.css";
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
 function getRandomAnimal() {
-  const animals = ["cat", "dog", "fish", "bird"];
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
   return animals[Math.floor(Math.random() * animals.length)];
 }
 
-// console.log(getRandomAnimal());
-
 function App() {
-  // Array Destructuring
-  // function makeArray() {
-  //   return [1, 10, 32, 40];
-  // }
-
-  // // const myArray = makeArray();
-  // // const firstElement = myArray[0];
-  // // const secondElement = myArray[1];
-  // // console.log(firstElement, secondElement);
-  // const [firstElement, secondElement] = makeArray();
-  // console.log(firstElement, secondElement);
-
   const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
     setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
+
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>{animals}</div>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
--- modern-react-with-redux/animals/src/index.js
+++ modern-react-with-redux/animals/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import AnimalShow from "./AnimalShow";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
