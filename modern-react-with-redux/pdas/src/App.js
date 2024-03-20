d0634b65954164d1f39d8bba5d6944e61bbf5c4f
Ishan <ishannaik7@gmail.com>
2024-03-20 04:20:31 +0000

    Last bit of styling for the ProfileCard component. Also, added a new prop to the ProfileCard component to allow for the display of the description of the Assistant.

--- modern-react-with-redux/pdas/src/App.js
+++ modern-react-with-redux/pdas/src/App.js
@@ -11,22 +11,41 @@ console.log(SiriImage);
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+          <p className="subtitle">
+            Compare the most popular personal digital assistants
+          </p>
+        </div>
+      </section>
+
       <div className="container">
         <section className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by amazon and helps you buy things."
+              />
             </div>
             <div className="column is-4">
               <ProfileCard
                 title="Cortana"
                 handle="@cortana32"
                 image={CortanaImage}
+                description="Cortona was created by Microsoft and helps you with your daily tasks."
               />
             </div>
             <div className="column is-4">
-              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by apple and is now being phased out"
+              />
             </div>
           </div>
         </section>
