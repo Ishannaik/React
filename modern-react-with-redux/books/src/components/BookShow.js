5d4fd8a5bf08bc648a1d09418c0f8c53cd121209
Ishan <ishannaik7@gmail.com>
2024-03-25 13:30:33 +0000

    Added images

--- modern-react-with-redux/books/src/components/BookShow.js
+++ modern-react-with-redux/books/src/components/BookShow.js
@@ -18,10 +18,11 @@ function BookShow({ book, onDelete, onEdit }) {
 
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit onSubmit={handleSubmit}  book={book} />;
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
   }
   return (
     <div className="book-show">
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
