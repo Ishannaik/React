2d5271c10d66b83a62b4aedacf1a049f5bb9b958
Ishan <ishannaik7@gmail.com>
2024-04-12 16:46:31 +0530

    A small taste of reusable hooks

--- modern-react-with-redux/books/src/components/BookCreate.js
+++ modern-react-with-redux/books/src/components/BookCreate.js
@@ -1,9 +1,9 @@
-import { useState, useContext } from 'react';
-import BooksContext from '../context/books';
+import { useState } from "react";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookCreate() {
-  const [title, setTitle] = useState('');
-  const { createBook } = useContext(BooksContext);
+  const [title, setTitle] = useState("");
+  const { createBook } = useBooksContext();
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -12,7 +12,7 @@ function BookCreate() {
   const handleSubmit = (event) => {
     event.preventDefault();
     createBook(title);
-    setTitle('');
+    setTitle("");
   };
 
   return (
