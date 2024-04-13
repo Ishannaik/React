541f0c2af93f652b948d8f40195d6a0727e27dd4
Ishan <ishannaik7@gmail.com>
2024-04-13 19:49:05 +0530

    Component setup

--- modern-react-with-redux/comps/src/App.js
+++ modern-react-with-redux/comps/src/App.js
@@ -1,58 +1,23 @@
-import Button from "./components/Button";
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+// import Button from "./components/Button";
+// import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 import "./index.css";
+import Accordion from "./components/Accordion.js";
 function App() {
-  const handleClick = () => {
-    console.log("Hello! I am a button.");
-  };
-  return (
-    <div>
-      <div>
-        <Button success rounded outline onClick={handleClick} className="mb-5">
-          <GoBell />
-          Hello
-        </Button>
-      </div>
-      <div>
-        <Button primary onMouseEnter={handleClick}>
-          <GoCloudDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button danger>
-          <GoDatabase />
-          See deal
-        </Button>
-      </div>
-      <div>
-        <Button secondary onMouseLeave={handleClick}>
-          Something
-        </Button>
-      </div>
-      <div>
-        <Button warning>haha</Button>
-      </div>
-      <div>
-        <Button rounded>hahaha</Button>
-      </div>
-      <div>
-        <Button success primary rounded>
-          hahaha
-        </Button>
-      </div>
-      <div>
-        <Button outline primary rounded>
-          hahaha
-        </Button>
-      </div>
-      <div>
-        <Button success warning rounded>
-          hahaha
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      label: "What is React?",
+      content: "React is a front-end JavaScript framework.",
+    },
+    {
+      label: "Why use React?",
+      content: "React allows you to create reusable UI components.",
+    },
+    {
+      label: "How do you use React?",
+      content: "You use React by creating components.",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
