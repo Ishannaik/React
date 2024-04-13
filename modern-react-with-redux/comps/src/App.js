1c7066b70330bdcfe0877321e3d07b43e37a3d2c
Ishan <ishannaik7@gmail.com>
2024-04-13 19:52:02 +0530

    Reminder on building Lists

--- modern-react-with-redux/comps/src/App.js
+++ modern-react-with-redux/comps/src/App.js
@@ -5,14 +5,17 @@ import Accordion from "./components/Accordion.js";
 function App() {
   const items = [
     {
+      id: 1,
       label: "What is React?",
       content: "React is a front-end JavaScript framework.",
     },
     {
+      id: 2,
       label: "Why use React?",
       content: "React allows you to create reusable UI components.",
     },
     {
+      id: 3,
       label: "How do you use React?",
       content: "You use React by creating components.",
     },
