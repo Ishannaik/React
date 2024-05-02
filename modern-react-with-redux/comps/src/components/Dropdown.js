041370af398a4216d236e245f88f585632a5a04a
Ishan <ishannaik7@gmail.com>
2024-05-02 17:54:04 +0530

    Updated dropdown component

--- modern-react-with-redux/comps/src/components/Dropdown.js
+++ modern-react-with-redux/comps/src/components/Dropdown.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Dropdown = ({ options }) => {
-  return <div>DROPDOWN</div>;
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
 };
 
 export default Dropdown;
