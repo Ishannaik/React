ed62299db4d5a950858a7a31ce42e6aba7c669c5
Ishan <ishannaik7@gmail.com>
2024-05-02 18:15:08 +0530

    Changed the Dropdown

--- modern-react-with-redux/comps/src/components/Dropdown.js
+++ modern-react-with-redux/comps/src/components/Dropdown.js
@@ -1,13 +1,36 @@
 import React from "react";
-
+import { useState } from "react";
 const Dropdown = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (event) => {
+    // CLOSE THE DROPDOWN
+    setIsOpen(false);
+    // WHAT OPTION DID THE USER CLICK ON?
+    console.log(event);
+  };
+
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} className="item">
+      <div
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+        className="item"
+      >
         {option.label}
       </div>
     );
   });
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 };
 
 export default Dropdown;
