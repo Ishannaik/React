605f9167153ede2733af650d784c7c5f6fa8a5f1
Ishan <ishannaik7@gmail.com>
2024-05-02 18:22:00 +0530

    Controlled Component Implementation

--- modern-react-with-redux/comps/src/App.js
+++ modern-react-with-redux/comps/src/App.js
@@ -1,12 +1,21 @@
 import Dropdown from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
   const options = [
     { label: "The Color Red", value: "red" },
     { label: "The Color Green", value: "green" },
     { label: "A Shade of Blue", value: "blue" },
   ];
-  return <Dropdown options={options} />;
+  return (
+    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
 
 export default App;
--- modern-react-with-redux/comps/src/components/Dropdown.js
+++ modern-react-with-redux/comps/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selection, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -11,7 +11,7 @@ const Dropdown = ({ options }) => {
     // CLOSE THE DROPDOWN
     setIsOpen(false);
     // WHAT OPTION DID THE USER CLICK ON?
-    console.log(option);
+    onSelect(option);
   };
 
   const renderedOptions = options.map((option) => {
@@ -25,9 +25,14 @@ const Dropdown = ({ options }) => {
       </div>
     );
   });
+
+  let content = "Select...";
+  if (selection) {
+    content = selection.label;
+  }
   return (
     <div>
-      <div onClick={handleClick}>Select...</div>
+      <div onClick={handleClick}>{content}</div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
